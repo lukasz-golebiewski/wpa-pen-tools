18bca0551faa20acccc617c68a307cbdcda3033c
Lukasz Golebiewski <lukasz.golebiewski@gmail.com>
2016-12-30 18:54:26 +0100

    Rename home->mnt fix

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 docker build -t kali .
-docker run -P -it --net="host" --privileged -v `pwd`/home:/home --name wifi-kali kali bash
+docker run -P -it --net="host" --privileged -v `pwd`/mnt:/mnt --name wifi-kali kali bash
